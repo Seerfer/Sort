bc19631cd29d3030a26d5beea59c202e6fd399fc
Patryk <patrykost1998@gmail.com>
2021-02-02 13:25:27 +0100

    Improved making algorithms to be more readable

--- test/set_conditions.py
+++ test/set_conditions.py
@@ -4,12 +4,8 @@ import os
 FILE = "conditions.json"
 
 condictions = json.load(open(os.path.dirname(os.path.realpath(__file__)) + '/' + FILE, "r"))
-algorithms = []
-for key, value in condictions["algorithms"].items():
-    if value is True:
-        algorithms.append(key)
+algorithms = [item[0] for item in condictions["algorithms"].items() if item[1] is True]
 partsort = condictions["other"]["part"]
 number_of_arrays = condictions["number_of_arrays"]
 array_length = condictions["array_length"]
 reverse = condictions["other"]["reverse"]
-
